2c7a4788d4830f873d08a251a33b7d13b087a2b3
kamil <herbetko.kamil@gmail.com>
2021-03-20 17:12:43 +0100

    updating laboratory 3 project

--- AiSD_Laboratoria/Laboratoria_3/ArrayStack.java
+++ AiSD_Laboratoria/Laboratoria_3/ArrayStack.java
@@ -1,4 +1,59 @@
 package AiSD_Laboratoria.Laboratoria_3;
 
-public class ArrayStack {
+import AiSD_Ćwiczenia.Ćwiczenia_2_2.Exceptions.EmptyStackException;
+import AiSD_Ćwiczenia.Ćwiczenia_2_2.Exceptions.FullStackException;
+
+public class ArrayStack<T> implements IStack<T>{
+    private static final int DEFAULT_CAPACITY = 1024;
+    private T array[];
+    private int topIndex;
+
+    @SuppressWarnings("unchecked")
+    public ArrayStack(int initialSize) {
+        this.array = (T[]) (new Object[initialSize]);
+        this.topIndex = 0;
+    }
+
+    public ArrayStack(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return topIndex == 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return topIndex == array.length;
+    }
+
+    @Override
+    public T pop() throws EmptyStackException {
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        return array[--topIndex];
+    }
+
+    @Override
+    public void push(T elem) throws FullStackException {
+        if (isFull()){
+            throw new FullStackException();
+        }
+        array[topIndex++] = elem;
+    }
+
+    @Override
+    public int size() {
+        return topIndex;
+    }
+
+    @Override
+    public T top() throws EmptyStackException {
+        if (isEmpty()){
+            throw new EmptyStackException();
+        }
+        return array[topIndex-1];
+    }
 }
--- AiSD_Laboratoria/Laboratoria_3/Nawiasy.java
+++ AiSD_Laboratoria/Laboratoria_3/Nawiasy.java
@@ -1,4 +1,25 @@
 package AiSD_Laboratoria.Laboratoria_3;
 
+import java.util.Arrays;
+
 public class Nawiasy {
+
+    public static boolean nawiasyZrownowazone(String wyrażenie){
+        ArrayStack<Character> stack = new ArrayStack<Character>(wyrażenie.length());
+
+    }
+
+    public static boolean nawiasOtwierajacy(char ch){
+        if ((ch == '(') || (ch == '[') || (ch == '{')){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean nawiasZamykajacy(char ch){
+        if ((ch == '}') || (ch == ']') || (ch == ')')){
+            return true;
+        }
+        return false;
+    }
 }
