d3864a8fa7adb78fbecc326cf9f06fc54d9f41a5
kamil <herbetko.kamil@gmail.com>
2021-04-18 22:43:00 +0200

    creating AiSD laboratory 5 project

---
+++ AiSD_Laboratoria/Laboratoria_5/Auto.java
@@ -0,0 +1,126 @@
+package AiSD_Laboratoria.Laboratoria_5;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Auto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 4163796131816322049L;
+    public static int number = 0;
+    private int NR_SILNIKA;
+    private String marka;
+    private String typ;
+    private int data_produkcji;
+    private int cena;
+    private String kolor;
+    private int skladowanie;
+    private double przecena;
+
+    public Auto(String marka, String typ, int data_produkcji, int cena, String kolor, int skladowanie) {
+        ++number;
+        this.NR_SILNIKA = number;
+        this.marka = marka;
+        this.typ = typ;
+        this.data_produkcji = data_produkcji;
+        this.cena = cena;
+        this.kolor = kolor;
+        this.skladowanie = skladowanie;
+        if (skladowanie >= 15){
+            this.przecena = 0.85 * this.cena;
+        }
+        else if ((6 <= skladowanie) && (skladowanie < 15)){
+            this.przecena = 0.94 * this.cena;
+        }
+        else {
+            this.przecena = cena;
+        }
+    }
+
+    public int getNR_SILNIKA() {
+        return NR_SILNIKA;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public int getData_produkcji() {
+        return data_produkcji;
+    }
+
+    public void setData_produkcji(int data_produkcji) {
+        this.data_produkcji = data_produkcji;
+    }
+
+    public int getCena() {
+        return cena;
+    }
+
+    public void setCena(int cena) {
+        this.cena = cena;
+    }
+
+    public String getKolor() {
+        return kolor;
+    }
+
+    public void setKolor(String kolor) {
+        this.kolor = kolor;
+    }
+
+    public int getSkladowanie() {
+        return skladowanie;
+    }
+
+    public void setSkladowanie(int skladowanie) {
+        this.skladowanie = skladowanie;
+    }
+
+    public double getPrzecena() {
+        return przecena;
+    }
+
+    public void setPrzecena() {
+        if (this.skladowanie >= 15){
+            this.przecena = 0.85 * this.cena;
+        }
+        else if ((6 <= this.skladowanie) && (this.skladowanie < 15)){
+            this.przecena = 0.94 * this.cena;
+        }
+        else {
+            this.przecena = cena;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("| %-13d | %-11s | %-9s | %-14d | %-6d | %-12s | %-11d | %-6.2f |", this.NR_SILNIKA, this.marka, this.typ, this.data_produkcji, this.cena, this.kolor, this.skladowanie, this.przecena);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Auto auto = (Auto) o;
+        return NR_SILNIKA == auto.NR_SILNIKA && data_produkcji == auto.data_produkcji && cena == auto.cena && skladowanie == auto.skladowanie && Double.compare(auto.przecena, przecena) == 0 && Objects.equals(marka, auto.marka) && Objects.equals(typ, auto.typ) && Objects.equals(kolor, auto.kolor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(NR_SILNIKA, marka, typ, data_produkcji, cena, kolor, skladowanie, przecena);
+    }
+}
---
+++ AiSD_Laboratoria/Laboratoria_5/Main.java
@@ -0,0 +1,335 @@
+package AiSD_Laboratoria.Laboratoria_5;
+
+import AiSD_Ćwiczenia.Ćwiczenia_3.Classes.TwoWayCycledListWithSentinel;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        TwoWayCycledListWithSentinel<Auto> listaSamochodow = new TwoWayCycledListWithSentinel<>();
+        Scanner scanner = new Scanner(System.in);
+        String[] marki = {"BMW", "Mercedes", "Ford", "Fiat", "Nissan", "Opel", "Dodge", "Audi", "Volkswagen", "Skoda", "Dacia", "Volvo", "Lexus", "Bugatti", "Porsche"};
+        String[] typ = {"SUV", "Coupe", "Hatchback", "Sedan", "Kabriolet", "Combi", "Van", "Liftback", "Minivan"};
+        String[] kolory = {"Niebieski", "Czerwony", "Żółty", "Pomarańczowy", "Zielony", "Szary", "Czarny", "Biały", "Bordowy", "Szmaragdowy", "Fioletowy", "Różowy", "Grafitowy"};
+
+        // Main loop
+        boolean run = true;
+        while (run){
+            System.out.println("Wybierz opcję:");
+            System.out.println("1 - Utworzenie nowej bazy danych.");
+            System.out.println("2 - Odczyt bazy z pliku.");
+            System.out.println("3 - Wyświetlenie wszystkich rekordów.");
+            System.out.println("4 - Wyświetlanie danych jednego samochodu.");
+            System.out.println("5 - Dopisanie nowego samochodu.");
+            System.out.println("6 - Usunięcie samochodu z bazy");
+            System.out.println("7 - Aktualizowanie bazy danych");
+            System.out.println("8 - Wyświetlanie danych samochodu o podanej marce.");
+            System.out.println("9 - Wyświetlanie danych samochodów, wyprodukowanych w podanym roku.");
+            System.out.println("10 - Wyświetlenie danych samochodów o cenach poniżej podanej.");
+            System.out.println("11 - Zapis bazy do pliku.");
+            System.out.println("12 - zakończ");
+
+            switch (scanner.nextLine().strip()){
+                case "1" ->{
+                    utworzenieListy(listaSamochodow, random, marki, typ, kolory);
+                }
+                case "2" -> {
+                    listaSamochodow = odczytZPliku();
+                }
+                case "3" -> {
+                    wyswietlWszystkie(listaSamochodow);
+                }
+                case "4" -> {
+                    wyswietlJeden(listaSamochodow, scanner);
+                }
+                case "5" -> {
+                    dopisanieSamochodu(listaSamochodow,scanner);
+                }
+                case "6" -> {
+                    usunZBazy(listaSamochodow, scanner);
+                }
+                case "7" -> {
+                    aktualizowanieSamochodu(listaSamochodow, scanner);
+                }
+                case "8" -> {
+                    wyswietlanieOPodanejMarce(listaSamochodow, scanner);
+                }
+                case "9" -> {
+                    wyswietlanieOPodanymRoku(listaSamochodow, scanner);
+                }
+                case "10" -> {
+                    wyswietlanieOCenieNizszejNizPodana(listaSamochodow, scanner);
+                }
+                case "11" -> {
+                    zapisDoPliku(listaSamochodow);
+                }
+                case "12" -> {
+                    run = false;
+                    scanner.close();
+                    return;
+                }
+            }
+
+            System.out.println();
+        }
+        switch (scanner.nextLine().strip()){
+            case "1" ->{
+                utworzenieListy(listaSamochodow, random, marki, typ, kolory);
+            }
+            case "2" -> {
+                listaSamochodow = odczytZPliku();
+            }
+            case "3" -> {
+                wyswietlWszystkie(listaSamochodow);
+            }
+            case "4" -> {
+                wyswietlJeden(listaSamochodow, scanner);
+            }
+            case "5" -> {
+                dopisanieSamochodu(listaSamochodow,scanner);
+            }
+            case "6" -> {
+                usunZBazy(listaSamochodow, scanner);
+            }
+            case "7" -> {
+                aktualizowanieSamochodu(listaSamochodow, scanner);
+            }
+            case "8" -> {
+                wyswietlanieOPodanejMarce(listaSamochodow, scanner);
+            }
+            case "9" -> {
+                wyswietlanieOPodanymRoku(listaSamochodow, scanner);
+            }
+            case "10" -> {
+                wyswietlanieOCenieNizszejNizPodana(listaSamochodow, scanner);
+            }
+            case "11" -> {
+                zapisDoPliku(listaSamochodow);
+            }
+            case "12" -> {
+                run = false;
+                scanner.close();
+                return;
+            }
+        }
+
+        System.out.println();
+    }
+
+    public static void utworzenieListy(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Random random, String[] marki, String[] typ, String[] kolory){
+        Auto.number = 0;
+        listaSamochodow.clear();
+        for (int i = 0; i < 40 + random.nextInt(40); i++) {
+            listaSamochodow.insert(new Auto(marki[random.nextInt(marki.length)], typ[random.nextInt(typ.length)], 1990 + random.nextInt(32), 5000 + random.nextInt(900000), kolory[random.nextInt(kolory.length)], random.nextInt(25)));
+        }
+    }
+
+    public static void wyswietlWszystkie(TwoWayCycledListWithSentinel<Auto> listaSamochodow){
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+        System.out.printf("| %-13s | %-11s | %-9s | %-14s | %-6s | %-12s | %-11s | %-8s |%n", "Numer silnika", "Marka", "Typ", "Data produkcji", "Cena", "Kolor", "Składowanie", "Przecena");
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString());
+        }
+    }
+
+    public static void wyswietlJeden(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj numer silnika samochodu który chcesz wyświetlić");
+        int numer = get_integer(scanner, "Podaj numer silnika samochodu który chcesz wyświetlić");
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+        while (iterator.hasNext()){
+            Auto auto = iterator.next();
+            if (auto.getNR_SILNIKA() == numer){
+                System.out.println(auto.toString());
+            }
+
+        }
+    }
+
+    public static void dopisanieSamochodu(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj markę samochodu");
+        String marka = scanner.nextLine().strip();
+
+        System.out.println("Podaj typ samochodu");
+        String typ = scanner.nextLine().strip();
+
+        System.out.println("Podaj datę produkcji samochodu");
+        int data = get_integer(scanner, "Podaj datę produkcji samochodu");
+
+        System.out.println("Podaj cenę samochodu");
+        int cena = get_integer(scanner, "Podaj cenę samochodu");
+
+        System.out.println("Podaj kolor samochodu");
+        String kolor = scanner.nextLine().strip();
+
+        System.out.println("Podaj czas składowania samochodu");
+        int czas = get_integer(scanner, "Podaj czas składowania samochodu");
+
+        listaSamochodow.insert(new Auto(marka, typ, data, cena, kolor, czas));
+    }
+
+    public static void usunZBazy(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj numer silnika samochodu który chcesz usunąć");
+        int numer = get_integer(scanner, "Podaj numer silnika samochodu który chcesz usunąć");
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+        while (iterator.hasNext()){
+            Auto auto = iterator.next();
+            if (auto.getNR_SILNIKA() == numer){
+                listaSamochodow.delete(auto);
+                return;
+            }
+        }
+        System.out.println("Nie znaleziono samochodu o podanym numerze silnika");
+    }
+
+    public static void aktualizowanieSamochodu(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj numer silnika samochodu który chcesz zaktualizować");
+        int numer = get_integer(scanner, "Podaj numer silnika samochodu który chcesz zaktualizować");
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+        while (iterator.hasNext()){
+            Auto auto = iterator.next();
+            if (auto.getNR_SILNIKA() == numer){
+                boolean run = true;
+                while (run){
+                    System.out.println("Wybierz cechę do zaktualizowania:");
+                    System.out.println("1 - Marka");
+                    System.out.println("2 - Typ");
+                    System.out.println("3 - Data produkcji");
+                    System.out.println("4 - Cena");
+                    System.out.println("5 - Kolor");
+                    System.out.println("6 - Czas składowania");
+                    System.out.println("7 - Powrtót");
+
+                    String wybór = scanner.nextLine().strip();
+                    switch (wybór){
+                        case "1" -> {
+                            System.out.println("Podaj markę");
+                            auto.setMarka(scanner.nextLine().strip());
+                        }
+                        case "2" -> {
+                            System.out.println("Podaj typ");
+                            auto.setTyp(scanner.nextLine().strip());
+                        }
+                        case "3" -> {
+                            System.out.println("Podaj datę produkcji");
+                            auto.setData_produkcji(get_integer(scanner, "Podaj datę produkcji"));
+                        }
+                        case "4" -> {
+                            System.out.println("Podaj cenę");
+                            auto.setCena(get_integer(scanner, "Podaj cenę"));
+                            auto.setPrzecena();
+                        }
+                        case "5" -> {
+                            System.out.println("Podaj kolor");
+                            auto.setKolor(scanner.nextLine().strip());
+                        }
+                        case "6" -> {
+                            System.out.println("Podaj czas składowania");
+                            auto.setSkladowanie(get_integer(scanner, "Podaj czas składowania"));
+                            auto.setPrzecena();
+                        }
+                        case "7" -> {
+                            return;
+                        }
+                    }
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println("Nie znaleziono samochodu o podanym numerze silnika");
+    }
+
+    public static void wyswietlanieOPodanejMarce(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj markę samochodów które chcesz wyświetlić");
+        String marka = scanner.nextLine().strip();
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+
+        while (iterator.hasNext()){
+            Auto auto = iterator.next();
+            if (auto.getMarka().equals(marka)){
+                System.out.println(auto.toString());
+            }
+        }
+    }
+
+    public static void wyswietlanieOPodanymRoku(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj rok produkcji samochodów które chcesz wyświetlić");
+        int data = get_integer(scanner, "Podaj rok produkcji samochodów które chcesz wyświetlić");
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+
+        while (iterator.hasNext()){
+            Auto auto = iterator.next();
+            if (auto.getData_produkcji() == data){
+                System.out.println(auto.toString());
+            }
+        }
+    }
+
+    public static void wyswietlanieOCenieNizszejNizPodana(TwoWayCycledListWithSentinel<Auto> listaSamochodow, Scanner scanner){
+        System.out.println("Podaj cenę poniżej której samochody chcesz wyświetlić");
+        int cena = get_integer(scanner, "Podaj cenę poniżej której samochody chcesz wyświetlić");
+
+        Iterator<Auto> iterator = listaSamochodow.iterator();
+
+        while (iterator.hasNext()){
+            Auto auto = iterator.next();
+            if (auto.getPrzecena() < cena){
+                System.out.println(auto.toString());
+            }
+        }
+    }
+
+    public static TwoWayCycledListWithSentinel<Auto> odczytZPliku(){
+        TwoWayCycledListWithSentinel<Auto> lista = new TwoWayCycledListWithSentinel<>();
+
+        try(ObjectInputStream is = new ObjectInputStream(new FileInputStream("lista.ser"))) {
+            Auto.number = is.read();
+            lista = (TwoWayCycledListWithSentinel<Auto>) is.readObject();
+
+
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+
+    public static void zapisDoPliku(TwoWayCycledListWithSentinel<Auto> listaSamochodow){
+        try(ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("lista.ser"))) {
+            os.write(Auto.number);
+            os.writeObject(listaSamochodow);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int get_integer(Scanner scanner, String tekst){
+        int integer = 0;
+        boolean loop = true;
+
+        while (loop){
+            try {
+                integer = scanner.nextInt();
+                if (integer < 0){
+                    throw new IllegalArgumentException();
+                }
+                scanner.nextLine();
+                loop = false;
+            }
+            catch (Exception e) {
+                scanner.nextLine();
+                System.out.println();
+                System.out.println(tekst);
+            }
+        }
+
+        return integer;
+    }
+
+}
